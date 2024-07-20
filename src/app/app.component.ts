74fbe09ddd18b7d48047627815947f5f95ff9ee3
MateusMarchiDev <92895006+MateusMarchiDev@users.noreply.github.com>
2024-07-20 17:46:40 -0300

    ajuste rota

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
-import { Component, AfterViewInit, OnDestroy } from '@angular/core';
+import { Component, AfterViewInit, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterViewInit, OnDestroy {
+export class AppComponent implements AfterViewInit, OnDestroy, OnInit {
+  constructor(private router: Router) {}
+  ngOnInit(): void {
+    this.router.navigate(['/']);
+  }
+  ngOninit(){
 
+  }
 
   ngAfterViewInit() {
 
